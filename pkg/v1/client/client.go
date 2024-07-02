fca1cf857d8cd41e9ac45f5a81ee9e2da6dd4238
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-02 21:13:09 +0100

    Update client.go

--- pkg/v1/client/client.go
+++ pkg/v1/client/client.go
@@ -1,135 +1,47 @@
-package sess
+package client
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
+	"crypto/tls"
 	"net/http"
-	"net/http/httputil"
-	"strings"
+	"time"
 
+	api "github.com/tdrip/apiclient/pkg/v1/api"
 	cfg "github.com/tdrip/apiclient/pkg/v1/cfg"
-	uris "github.com/tdrip/apiclient/pkg/v1/uris"
+	sess "github.com/tdrip/apiclient/pkg/v1/session"
 )
 
-type Session struct {
-	accesstoken  string
-	refreshtoken string
-	api          cfg.APIServer
-	auth         cfg.AuthServer
-	client       *http.Client
-	Debug        bool
-	DumpResponse bool
-	DumpRequest  bool
+type Client struct {
+	Session *sess.AuthorisedSession
+	APIs    map[string]api.API
 }
 
-func NewSession(client *http.Client, api cfg.APIServer, auth cfg.AuthServer) (Session, error) {
-	sess := Session{}
-	sess.client = client
-	sess.api = api
-	sess.auth = auth
-	return sess, nil
-}
-
-func (sess Session) PostBody(uri string, req interface{}) ([]byte, *http.Response, error) {
-	return sess.AuthorizedRequest(http.MethodPost, uri, sess.api.EndPoint, req)
-}
-
-func (sess Session) HeadBody(uri string, req interface{}) ([]byte, *http.Response, error) {
-	return sess.AuthorizedRequest(http.MethodHead, uri, sess.api.EndPoint, req)
-}
-
-func (sess Session) Get(uri string) ([]byte, *http.Response, error) {
-	return sess.AuthorizedRequest(http.MethodGet, uri, sess.api.EndPoint, nil)
-}
-
-func (sess Session) GetBody(uri string, req interface{}) ([]byte, *http.Response, error) {
-	return sess.AuthorizedRequest(http.MethodGet, uri, sess.api.EndPoint, req)
-}
-
-func (sess Session) PutBody(uri string, req interface{}) ([]byte, *http.Response, error) {
-	return sess.AuthorizedRequest(http.MethodPut, uri, sess.api.EndPoint, req)
-}
-
-func (sess Session) AuthorizedRequest(method string, uri string, ep uris.EndPoint, req interface{}) ([]byte, *http.Response, error) {
-
-	url, err := ep.GetURL(uri)
-	emptydata := []byte{}
-	if err != nil {
-		return emptydata, nil, err
+func buildClient() *http.Client {
+	transport := &http.Transport{Proxy: http.ProxyFromEnvironment, TLSClientConfig: &tls.Config{Renegotiation: tls.RenegotiateOnceAsClient, InsecureSkipVerify: true}, TLSNextProto: map[string]func(authority string, c *tls.Conn) http.RoundTripper{}}
+	return &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: transport,
 	}
-	return sess.Authorized(method, url, req)
 }
 
-func (sess Session) Authorized(method string, url string, req interface{}) ([]byte, *http.Response, error) {
-	emptydata := []byte{}
-
-	headers := make(map[string]string)
-	headers["Content-Type"] = "application/json"
-	headers["Authorization"] = "Bearer " + sess.accesstoken
-
-	res, err := sess.APICall(method, url, req, headers)
-
-	if err != nil {
-		return emptydata, res, err
-	}
-
-	if res == nil {
-		return emptydata, res, fmt.Errorf("%s result was nil: %s Status Code %d", url, res.Status, res.StatusCode)
-	}
-
-	if sess.Debug {
-		debug(httputil.DumpResponse(res, sess.DumpResponse))
-	}
-
-	if res.StatusCode != 200 {
-		return emptydata, res, fmt.Errorf("%s failed with Status: %s Status Code %d", url, res.Status, res.StatusCode)
-	}
-
-	if res.Body != http.NoBody {
-		defer res.Body.Close()
-		bytes, err := ioutil.ReadAll(res.Body)
-		return bytes, res, err
-	}
-	// return empty if got no body, response and err
-	return []byte{}, res, err
+func NewTlsSkip(authtoken string, api cfg.APIServer, auth cfg.AuthServer) (*Client, error) {
+	return New(buildClient(), authtoken, api, auth)
 }
 
-func (sess Session) APICall(method string, url string, body interface{}, headers map[string]string) (*http.Response, error) {
+func New(cl *http.Client, authtoken string, api cfg.APIServer, auth cfg.AuthServer) (*Client, error) {
+	client := Client{}
 
-	cs, err := json.Marshal(body)
+	session, err := sess.NewAuthorisedSession(cl, api, auth)
 	if err != nil {
 		return nil, err
 	}
-
-	tosent := string(cs)
-	payload := strings.NewReader(tosent)
-	req, reqerr := http.NewRequest(method, url, payload)
-
-	if reqerr != nil {
-		return nil, reqerr
-	}
-
-	for k, v := range headers {
-		req.Header.Add(k, v)
-	}
-
-	if sess.Debug {
-		debug(httputil.DumpRequestOut(req, sess.DumpRequest))
-	}
-
-	if sess.client == nil {
-		return http.DefaultClient.Do(req)
-	} else {
-		return sess.client.Do(req)
-	}
+	session = session.UpdateAToken(authtoken)
+	client.Session = &session
+	return &client, nil
 }
 
-func debug(data []byte, err error) {
-	if err == nil {
-		log.Printf("%s\n\n", data)
-	} else {
-		log.Fatalf("%s\n\n", err)
-	}
+func (cl Client) AddAPI(a api.API) Client {
+	apis := cl.APIs
+	apis[a.GetName()] = a
+	cl.APIs = apis
+	return cl
 }
