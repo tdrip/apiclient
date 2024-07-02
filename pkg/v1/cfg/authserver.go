5eb8143a0374ce89b07df6e45f5ec6138e1145bb
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-02 16:30:03 +0100

    Update authserver.go

--- pkg/v1/cfg/authserver.go
+++ pkg/v1/cfg/authserver.go
@@ -9,3 +9,25 @@ type AuthServer struct {
 	Verifyauth string
 	Revokeauth string
 }
+
+func NewAuthServer(server string, authpath string) (AuthServer, error) {
+	ai := AuthServer{}
+	api, err := uris.NewEndPoint(server, authpath)
+	if err != nil {
+		return ai, err
+	}
+	ai.EndPoint = api
+	return ai, nil
+}
+
+func NewAuthServerWithVerify(server string, authpath string, verifyauth string, revokeauth string) (AuthServer, error) {
+	ai := AuthServer{}
+	api, err := uris.NewEndPoint(server, authpath)
+	if err != nil {
+		return ai, err
+	}
+	ai.EndPoint = api
+	ai.Revokeauth = revokeauth
+	ai.Verifyauth = verifyauth
+	return ai, nil
+}
