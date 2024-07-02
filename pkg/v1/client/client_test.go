9ee08fdd0e00d1b2909dc53300049bc7c22f9875
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-02 21:14:05 +0100

    Create client_test.go

---
+++ pkg/v1/client/client_test.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"fmt"
+	"testing"
+
+	cfg "github.com/tdrip/apiclient/pkg/v1/cfg"
+)
+
+func TestClient(t *testing.T) {
+	auth := cfg.AuthServer{}
+	api, _ := cfg.NewAPIServer("jsonplaceholder.typicode.com", "todos")
+	newclient, err := NewTlsSkip("", api, auth)
+	if err != nil {
+		t.Errorf("%v", err.Error())
+	}
+
+	newclient.Session.Debug = true
+	newclient.Session.DumpResponse = true
+	newclient.Session.DumpRequest = true
+
+	bytes, resp, err := newclient.Session.Get("/1")
+	if err != nil {
+		t.Fatalf("%v", err.Error())
+	}
+
+	fmt.Println("Bytes")
+	fmt.Printf("%s", string(bytes))
+	fmt.Println("Response")
+	fmt.Printf("%v", resp)
+}
