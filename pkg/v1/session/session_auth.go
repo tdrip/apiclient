42bc958e9b3b9f9a0bd3d5908c4dcd10f2abbd53
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-03 10:21:51 +0100

    Update session_auth.go

--- pkg/v1/session/session_auth.go
+++ pkg/v1/session/session_auth.go
@@ -2,9 +2,7 @@ package sess
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/http/httputil"
 
 	cfg "github.com/tdrip/apiclient/pkg/v1/cfg"
 	uris "github.com/tdrip/apiclient/pkg/v1/uris"
@@ -12,9 +10,10 @@ import (
 
 type AuthorisedSession struct {
 	Session
-	accesstoken  string
-	refreshtoken string
-	auth         cfg.AuthServer
+	accesstoken    string
+	refreshtoken   string
+	auth           cfg.AuthServer
+	GetAuthHeaders GetHeaders
 }
 
 func NewAuthorisedSessionCustomLogger(client *http.Client, api cfg.APIServer, auth cfg.AuthServer, logger SessionLog) (AuthorisedSession, error) {
@@ -28,6 +27,7 @@ func NewAuthorisedSession(client *http.Client, api cfg.APIServer, auth cfg.AuthS
 	sess, err := NewSession(client, api)
 	asess.Session = sess
 	asess.auth = auth
+	asess.GetAuthHeaders = asess.AuthorisedHeaders()
 	return asess, err
 }
 
@@ -58,47 +58,17 @@ func (asess AuthorisedSession) AuthorizedRequest(method string, uri string, ep u
 	if err != nil {
 		return emptydata, nil, err
 	}
-	return asess.Authorized(method, url, req)
+	return asess.Session.Call(method, url, req, asess.GetAuthHeaders)
 }
 
 func (asess AuthorisedSession) AuthorisedHeaders() map[string]string {
 
 	headers := make(map[string]string)
 	headers["Content-Type"] = "application/json"
+	headers["Accept"] = "application/json"
 	headers["Authorization"] = "Bearer " + asess.accesstoken
 
 	return headers
-}
-
-func (asess AuthorisedSession) Authorized(method string, url string, req interface{}) ([]byte, *http.Response, error) {
-	emptydata := []byte{}
-
-	res, err := asess.Session.APICall(method, url, req, asess.AuthorisedHeaders())
-
-	if err != nil {
-		return emptydata, res, err
-	}
-
-	if res == nil {
-		return emptydata, res, fmt.Errorf("%s result was nil: %s Status Code %d", url, res.Status, res.StatusCode)
-	}
-
-	if asess.Session.Debug && asess.Session.Logger != nil {
-		b, e := httputil.DumpResponse(res, asess.Session.DumpResponse)
-		asess.Session.Logger("Authorized", b, e)
-	}
-
-	if res.StatusCode != 200 {
-		return emptydata, res, fmt.Errorf("%s failed with Status: %s Status Code %d", url, res.Status, res.StatusCode)
-	}
-
-	if res.Body != http.NoBody {
-		defer res.Body.Close()
-		bytes, err := ioutil.ReadAll(res.Body)
-		return bytes, res, err
-	}
-	// return empty if got no body, response and err
-	return []byte{}, res, err
 }
 
 func (asess AuthorisedSession) UpdateAToken(accesstoken string) AuthorisedSession {
